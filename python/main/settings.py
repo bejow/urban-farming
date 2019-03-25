3b30b6e2b8662711788eadb2891432cd036cf340
Bejowe <fast.pfeil@googlemail.com>
2019-03-25 12:33:15 +0100

    seperated hardcoded values into settings file

--- python/main/settings.py
+++ python/main/settings.py
@@ -1,10 +1,20 @@
 #default settings for the Plants
-default_settings = {
+system_settings = {
     "light_time": 7200,
     "no_light_time" : 7200,
     "water_time" : 5,
     "no_water_time": 2, 
 }
+
+#programm settings
+
+update_ph_frequency = 60
+update_oxygen_frequency = 60
+update_temperature_frequency = 60
+fetch_settings_frequency = 3
+water_relais_pin = 13
+light_relais_pin = 11
+
 
 #api settings
 api_url = "http://192.168.1.21:3000/"
